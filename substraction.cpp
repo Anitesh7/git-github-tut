4d6f377e9bcd1e521b78666c2dd2bd4b66c4261e
Anitesh <aniteshbarman857@gmail.com>
2022-01-06 23:16:57 +0530

    added new Substraction.cpp file

---
+++ substraction.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {    
+
+    int number1, number2, sub;
+    
+    cout<<"Enter two integers: "<<endl;
+    cin>>number1>>number2;
+
+    // calculating sum
+    sub = number1 - number2;      
+    
+    cout<<"substraction : "<<sub;
+    return 0;
+}
